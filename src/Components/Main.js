8daf751e2dd976df1ff2cb227cb95464d1142109
Estevão <estevaog.gomes@gmail.com>
2022-03-06 12:58:36 -0300

    Edited styles and added new images

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,63 +2,73 @@ import React from "react"
 import { Col, Row, Image } from "react-bootstrap"
 import About from "./images/About.jpg"
 import Material from "./images/Material.jpeg"
-import Methodology from "./images/Methodology.jpg"
+import Broward from "./images/Broward.png"
 
 export default function Main(){
     return(
         <div className="main">
-            <Row>
-                <Col>
-                    <div className="aboutText">
-                        <h1>Here comes some text about Fernanda Santos</h1>
-                    </div>
-                </Col>
-                <Col>
-                    <div className="aboutImage">
-                        <img  src={About} />
-                    </div>
-                </Col>
-            </Row>
-            <div className="whiteBg">
+            <div className="blueBg">
+                <Row>
+                    <Col>
+                        <div className="aboutText">
+                            <h1>Here comes some text about Fernanda Santos</h1>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="aboutImage">
+                            <img  src={About} />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <div className="broward">
                 <Row>
                     <Col>
                         <div className="personalText"> 
-                            <h1>Here comes some text about personal learning</h1>
+                            <h1>Here comes some text about being Broward Embassador</h1>
                         </div>
                     </Col>
                     <Col>
                         <div className="methodologyImage">
-                            <img src={Methodology}/>
+                            <img src={Broward} />
                         </div>
                     </Col>
                     <Col>
                         <div className="methodologyText"> 
-                            <h1>Here comes some text about Methodology</h1>
+                            <h1>Here comes some text about Methodology implemented about personal learning</h1>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <div className="blueBg2">
+                <Row>
+                    <Col>
+                        <div className="materialImage">
+                            <img src={Material}/>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="materialText">
+                            <h1>Here comes some text about the Material</h1>
                         </div>
                     </Col>
                 </Row>
             </div>
             <Row>
                 <Col>
-                    <div className="materialText">
-                        <h1>Here comes some text about the Material</h1>
+                    <div className="fourthRowText">
+                        <h1>Here comes The Contact Information</h1>
                     </div>
                 </Col>
                 <Col>
-                    <div className="materialImage">
-                        <img src={Material}/>
+                    <div className="fourthRowEmail">
+                        <h4>Here comes The Email Information</h4>
+                    </div>
+                    <div className="fourthRowInsta">
+                        <h4>Here comes The Instagram Information</h4>
                     </div>
                 </Col>
             </Row>
-            <div className="whiteBg">
-                <Row>
-                    <Col>
-                        <div className="fourthRowText">
-                            <h1>Here comes The Contact Information</h1>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
         </div>
     )
 }
